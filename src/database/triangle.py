68c3da3b590322fa7f1da3f05aeeb7a32f06002e
Karlos Helton Braga <karlos.hbraga@gmail.com>
2023-01-28 01:14:06 +0000

    Add endpoint to get history from database

--- src/database/triangle.py
+++ src/database/triangle.py
@@ -1,7 +1,11 @@
 from sqlmodel import SQLModel, Field
 
-class Triangle(SQLModel, table=True):
-	id: int = Field(primary_key=True)
+
+class TriangleBase(SQLModel):
 	side1: int
 	side2: int
 	side3: int
+
+
+class Triangle(TriangleBase, table=True):
+	id: int = Field(primary_key=True)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from typing import Optional
 from fastapi import Depends, FastAPI
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 import sys
 
-from database.triangle import Triangle
+from database.triangle import Triangle, TriangleBase
 sys.path.insert(1, '/src')
 
 from database.base import init_db, get_session
@@ -23,23 +24,31 @@ async def status():
     return "Ok"
 
 
-@app.get("/api/triangle")
-async def post_triangle(side1:int, side2:int, side3:int, session: AsyncSession = Depends(get_session)):
-    triangle = Triangle()
-    triangle.side1 = side1
-    triangle.side2 = side2
-    triangle.side3 = side3
+@app.post("/api/triangles")
+async def post_triangle(triangle: TriangleBase, session: AsyncSession = Depends(get_session)):
+    triangle = Triangle(
+        side1 = triangle.side1,
+        side2 = triangle.side2,
+        side3 = triangle.side3,
+    )
     session.add(triangle)
     session.commit()
     session.refresh(triangle)
 
-    if side1 == side2 and side1 == side3:
+    if triangle.side1 == triangle.side2 and triangle.side1 == triangle.side3:
         triangle_type = "equilateral"
 
-    elif side1 == side2 or side1 == side3 or side2 == side3:
+    elif triangle.side1 == triangle.side2 or triangle.side1 == triangle.side3 or triangle.side2 == triangle.side3:
         triangle_type = "isosceles"
 
     else:
         triangle_type = "scalene"
 
     return {"triangle type":triangle_type}
+
+
+@app.get("/api/triangles")
+async def get_triangle(session: AsyncSession = Depends(get_session)):
+    result = session.execute(select(Triangle))
+
+    return result.scalars().all()
