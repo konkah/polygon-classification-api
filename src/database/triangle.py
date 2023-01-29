44d0098c85b146e634506eed39b0be29fa8ea315
Karlos Helton Braga <karlos.hbraga@gmail.com>
2023-01-29 12:35:54 +0000

    Fix json id

--- src/database/triangle.py
+++ src/database/triangle.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Field
 from pydantic import PositiveInt
 
 class TriangleBase(SQLModel):
-	id: int
+	id: int = 0
 	side1: PositiveInt
 	side2: PositiveInt
 	side3: PositiveInt
